89323e6390e4612fab3bec5f95cbdfb9dd039752
srhtgdng <serhatgnn1@hotmail.com>
2020-07-28 19:33:28 +0300

    a

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,19 @@
 const app = new Vue({
     el: '#app',
     data: {
-      message: 'You loaded this page on ' + new Date().toLocaleString(),
       config: "",
-      blog: []
+      blog: [],
+      port: []
     },
     created(){
-        fetch('./config.json')
+        fetch('config.json')
         .then((res) => {  
         this.config = res.json(); 
         return this.config
         }).then((res) => {
             this.blog = res.blogs;
+            this.port = res.port;
+            console.log(res);
         })
     }
   })
